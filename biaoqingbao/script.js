dbfe29315dc10840da1e7b1e50e56d6c11e53f49
huangshanshu4 <156116351+huangshanshu4@users.noreply.github.com>
2024-03-02 16:00:06 +0800

    3

---
+++ biaoqingbao/script.js
@@ -0,0 +1,6 @@
+// script.js
+
+function toggleDrawer() {
+  var drawer = document.querySelector('.drawer');
+  drawer.style.display = (drawer.style.display === 'none' || drawer.style.display === '') ? 'flex' : 'none';
+}
