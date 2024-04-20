6e1685d0b5c5b8bd1a2429037b0ece88b947e046
huangshanshu4 <156116351+huangshanshu4@users.noreply.github.com>
2024-04-20 13:20:20 +0800

    2

--- css/script.js
+++ css/script.js
@@ -1,6 +1,18 @@
+const spans = document.querySelectorAll('#text-container span');
+let index = 0;
+
+function fadeIn() {
+  if (index < spans.length) {
+    spans[index].style.animation = 'fadeInAnimation 2s ease forwards';
+    index++;
+  }
+}
+
 // script.js
 
 function toggleDrawer() {
   var drawer = document.querySelector('.drawer');
   drawer.style.display = (drawer.style.display === 'none' || drawer.style.display === '') ? 'flex' : 'none';
 }
+
+setTimeout(fadeIn, 1000);
